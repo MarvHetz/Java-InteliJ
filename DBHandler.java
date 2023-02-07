5e21449db730a50d58f1773e779b07e0dc34e4e5
MarvHetz <116543408+MarvHetz@users.noreply.github.com>
2023-02-07 17:03:30 +0100

    DBHandler funktioniert

--- DBHandler.java
+++ DBHandler.java
@@ -1,13 +1,21 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBHandler
 {
 	Connection connection;
-	public DBHandler(){
+
+	public DBHandler()
+	{
 		// Load the UCANACCESS JDBC driver
-		try {
+		try
+		{
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-		} catch (ClassNotFoundException e) {
+		}
+		catch (ClassNotFoundException e)
+		{
 			System.out.println("UCANACCESS driver not found in classpath");
 			e.printStackTrace();
 			return;
@@ -15,39 +23,57 @@ public class DBHandler
 
 		// Connect to the Microsoft Access database using the UCANACCESS JDBC driver
 		connection = null;
-		try {
+		try
+		{
 			connection = DriverManager.getConnection("jdbc:ucanaccess://DB.accdb");
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			System.out.println("Failed to connect to the database");
 			e.printStackTrace();
 		}
 
 
-		createPlaylist("test");
 		closeConnection();
 
 	}
 
-	public void createPlaylist(String name){
+	private void openConnection()
+	{
+		try
+		{
+			connection = DriverManager.getConnection("jdbc:ucanaccess://DB.accdb");
+		}
+		catch (SQLException e)
+		{
+			System.out.println("Failed to connect to the database");
+			e.printStackTrace();
+		}
+	}
+
+	public void createPlaylist(String name)
+	{
+		openConnection();
 		try
 		{
 			Statement stmt = connection.createStatement();
-			stmt.executeQuery("CREATE TABLE " + name + " (id int PRIMARY KEY, name VARCHAR(255), path VARCHAR(255));");
+			stmt.execute("CREATE TABLE " + name + " (id int PRIMARY KEY, name VARCHAR(255), path VARCHAR(255));");
 		}
 		catch (SQLException e)
 		{
 			System.out.println("Failed to create statement");
 			e.printStackTrace();
 		}
-
-
 	}
 
 	private void closeConnection()
 	{
-		try {
+		try
+		{
 			connection.close();
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			System.out.println("Failed to close the connection");
 			e.printStackTrace();
 		}
