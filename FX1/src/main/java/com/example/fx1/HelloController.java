de24e40672ef8a4b6987b3fd9eb0200631701c69
MarvHetz <hetzermarvin@dv-schulen.de>
2023-02-08 10:15:40 +0100

    tried fx

---
+++ FX1/src/main/java/com/example/fx1/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.fx1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController
+{
+    @FXML
+    public void berechnen()
+    {
+        System.out.println("Berechnen");
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# Musicplayer
-This is a little Musicplayer programmed in Java.
-
-# Adding new Music
-If you want to add new music by yourself you have to add it to Database and not just put it into the song folder.
