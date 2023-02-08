5d897f54e28e9cf55dc9f6b9e7f18fbcf671752b
MarvHetz <116543408+MarvHetz@users.noreply.github.com>
2023-02-08 19:58:28 +0100

    DBHandler loading all Playlists
    Created a function that loads all Playlists from the Playlists Table of the Database

--- Playlist.java
+++ Playlist.java
@@ -2,12 +2,18 @@ import java.util.ArrayList;
 
 public class Playlist<T extends Song>
 {
-    private final ArrayList<? extends Song> songs;
-    private final String titel;
-    
-    public Playlist(ArrayList<? extends Song> songs, String titel)
-    {
-        this.songs = songs;
-        this.titel = titel;
-    }
+	private final int id;
+	private final ArrayList<? extends Song> songs;
+	private final String titel;
+	public Playlist(int id, ArrayList<? extends Song> songs, String titel)
+	{
+		this.id = id;
+		this.songs = songs;
+		this.titel = titel;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
 }
