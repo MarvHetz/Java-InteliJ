d10129f22e692fca017ce30d29bca02a5cdb90a0
MarvHetz <hetzermarvin@dv-schulen.de>
2023-03-08 11:50:44 +0100

    mehrere Scenes und multilanguage über diese scenes hinweg

--- JavaFXUebung/src/main/java/com/example/javafxuebung/KontoUebersicht.java
+++ JavaFXUebung/src/main/java/com/example/javafxuebung/KontoUebersicht.java
@@ -1,5 +1,50 @@
 package com.example.javafxuebung;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.ResourceBundle;
+
 public class KontoUebersicht
 {
+    private static KontoUebersicht instance;
+    @FXML
+    private Label lblAccountOverview;
+
+    @FXML
+    private Label lblAccountNow;
+
+    @FXML
+    private Label lblCreditInterest;
+
+    @FXML
+    private Label lblAccountNowNumber;
+
+    @FXML
+    private Label lblCreditInterestNumber;
+
+    @FXML
+    private Button btnDeposit;
+
+    @FXML
+    private Button btnPayout;
+
+    @FXML
+    private TextField txtDeposit;
+
+    @FXML
+    private TextField txtPayout;
+
+
+    public static KontoUebersicht getInstance()
+    {
+        return instance;
+    }
+
+    public void setLanguage()
+    {
+        ResourceBundle bundle = ResourceBundle.getBundle("Language", Language.getLocale());
+    }
 }
